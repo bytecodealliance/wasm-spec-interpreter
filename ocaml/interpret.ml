656c01658471e445bad6710db31e8f924bb78966
Andrew Brown <andrew.brown@intel.com>
2022-08-19 09:12:19 -0700

    Initial commit
    This commit is a direct move from its original location in the [Wasmtime
    repository](https://github.com/bytecodealliance/wasmtime/tree/main/crates/fuzzing/wasm-spec-interpreter)
    to a separate repository for easier publishing. The only change is the
    addition of the top-level `.gitignore` file.

---
+++ ocaml/interpret.ml
@@ -0,0 +1,77 @@
+(* This module exposes an [interpret] function to Rust. It wraps several different calls from the
+WebAssembly specification interpreter in a way that we can access across the FFI boundary. To
+understand this better, see:
+ - the OCaml manual documentation re: calling OCaml from C, https://ocaml.org/manual/intfc.html#s%3Ac-advexample
+ - the [ocaml-interop] example, https://github.com/tezedge/ocaml-interop/blob/master/testing/rust-caller/ocaml/callable.ml
+*)
+
+(* Here we access the WebAssembly specification interpreter; this must be linked in. *)
+open Wasm
+open Wasm.WasmRef_Isa_m.WasmRef_Isa
+
+(** Enumerate the types of values we pass across the FFI boundary. This must match `Value` in
+`src/lib.rs` *)
+type ffi_value =
+  | I32 of int32
+  | I64 of int64
+  | F32 of int32
+  | F64 of int64
+  | V128 of Bytes.t
+
+(** Helper for converting the FFI values to their spec interpreter type. *)
+let convert_to_wasm (v: ffi_value) : v = match v with
+| I32 n -> V_num (ConstInt32 (I32_impl_abs n))
+| I64 n -> V_num (ConstInt64 (I64_impl_abs n))
+| F32 n -> V_num (ConstFloat32 (F32.of_bits n))
+| F64 n -> V_num (ConstFloat64 (F64.of_bits n))
+| V128 n -> V_vec (ConstVec128 (V128.of_bits (Bytes.to_string n)))
+
+(** Helper for converting the spec interpreter values to their FFI type. *)
+let convert_from_wasm (v: v) : ffi_value = match v with
+| V_num ((ConstInt32 (I32_impl_abs n))) -> I32 n
+| V_num ((ConstInt64 (I64_impl_abs n))) -> I64 n
+| V_num ((ConstFloat32 n)) -> F32 (F32.to_bits n)
+| V_num ((ConstFloat64 n)) -> F64 (F64.to_bits n)
+| V_vec ((ConstVec128 n)) -> V128 (Bytes.of_string (V128.to_bits n))
+| _ -> failwith "Unknown type"
+
+(** Parse the given WebAssembly module binary into an Ast.module_. At some point in the future this
+should also be able to parse the textual form (TODO). *)
+let parse bytes =
+  (* Optionally, use Bytes.unsafe_to_string here to avoid the copy *)
+  let bytes_as_str = Bytes.to_string bytes in
+  (Decode.decode "default" bytes_as_str)
+
+(** Return true if an export is a function. *)
+let match_exported_func export = match export with
+| Module_export_ext(_,Ext_func n,_) -> true
+| _ -> false
+
+(** Extract a function from its export or fail. *)
+let extract_exported_func export = match export with
+| Module_export_ext(_,Ext_func n,_) -> n
+| _ -> failwith ""
+
+(** Interpret the first exported function and return the result. Use provided
+parameters if they exist, otherwise use default (zeroed) values. *)
+let interpret_exn module_bytes opt_params =
+  let opt_params_ = Option.map (List.rev_map convert_to_wasm) opt_params in
+  let module_ = parse module_bytes in
+  let m_isa = Ast_convert.convert_module (module_.it) in
+  let fuel = Z.of_string "4611686018427387904" in
+  let max_call_depth = Z.of_string "300" in
+  (match run_fuzz (nat_of_integer fuel) (nat_of_integer max_call_depth) (make_empty_store_m ()) m_isa [] opt_params_ () with
+  | (s', RValue vs_isa') -> List.rev_map convert_from_wasm vs_isa'
+  | (s', RTrap str) -> raise (Eval.Trap (Source.no_region, "(Isabelle) trap: " ^ str))
+  | (s', (RCrash (Error_exhaustion str))) -> raise (Eval.Exhaustion (Source.no_region, "(Isabelle) call stack exhausted"))
+  | (s', (RCrash (Error_invalid str))) -> raise (Eval.Crash (Source.no_region, "(Isabelle) error: " ^ str))
+  | (s', (RCrash (Error_invariant str))) -> raise (Eval.Crash (Source.no_region, "(Isabelle) error: " ^ str))
+  (* TODO eventually we should hash the memory state and return the hash *)
+  )
+
+let interpret module_bytes opt_params =
+  try Ok(interpret_exn module_bytes opt_params) with
+  | _ as e -> Error(Printexc.to_string e)
+
+let () =
+  Callback.register "interpret" interpret;
---
+++ src/with_library.rs
@@ -0,0 +1,143 @@
+//! Interpret WebAssembly modules using the OCaml spec interpreter.
+//! ```
+//! # use wasm_spec_interpreter::{Value, interpret};
+//! let module = wat::parse_file("tests/add.wat").unwrap();
+//! let parameters = vec![Value::I32(42), Value::I32(1)];
+//! let results = interpret(&module, Some(parameters)).unwrap();
+//! assert_eq!(results, &[Value::I32(43)]);
+//! ```
+use crate::Value;
+use ocaml_interop::{OCamlRuntime, ToOCaml};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static INTERPRET: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
+
+/// Interpret the first function in the passed WebAssembly module (in Wasm form,
+/// currently, not WAT), optionally with the given parameters. If no parameters
+/// are provided, the function is invoked with zeroed parameters.
+pub fn interpret(module: &[u8], opt_parameters: Option<Vec<Value>>) -> Result<Vec<Value>, String> {
+    // The OCaml runtime is not re-entrant
+    // (https://ocaml.org/manual/intfc.html#ss:parallel-execution-long-running-c-code).
+    // We need  to make sure that only one Rust thread is executing at a time
+    // (using this lock) or we can observe `SIGSEGV` failures while running
+    // `cargo test`.
+    let _lock = INTERPRET.lock().unwrap();
+    // Here we use an unsafe approach to initializing the `OCamlRuntime` based
+    // on the discussion in https://github.com/tezedge/ocaml-interop/issues/35.
+    // This was the recommendation to resolve seeing errors like `boxroot is not
+    // setup` followed by a `SIGSEGV`; this is similar to the testing approach
+    // in
+    // https://github.com/tezedge/ocaml-interop/blob/master/testing/rust-caller/src/lib.rs
+    // and is only as safe as the OCaml code running underneath.
+    OCamlRuntime::init_persistent();
+    let ocaml_runtime = unsafe { OCamlRuntime::recover_handle() };
+    // Parse and execute, returning results converted to Rust.
+    let module = module.to_boxroot(ocaml_runtime);
+
+    let opt_parameters = opt_parameters.to_boxroot(ocaml_runtime);
+    let results = ocaml_bindings::interpret(ocaml_runtime, &module, &opt_parameters);
+    results.to_rust(ocaml_runtime)
+}
+
+// Here we declare which functions we will use from the OCaml library. See
+// https://docs.rs/ocaml-interop/0.8.4/ocaml_interop/index.html#example.
+mod ocaml_bindings {
+    use super::*;
+    use ocaml_interop::{
+        impl_conv_ocaml_variant, ocaml, OCamlBytes, OCamlInt32, OCamlInt64, OCamlList,
+    };
+
+    // Using this macro converts the enum both ways: Rust to OCaml and OCaml to
+    // Rust. See
+    // https://docs.rs/ocaml-interop/0.8.4/ocaml_interop/macro.impl_conv_ocaml_variant.html.
+    impl_conv_ocaml_variant! {
+        Value {
+            Value::I32(i: OCamlInt32),
+            Value::I64(i: OCamlInt64),
+            Value::F32(i: OCamlInt32),
+            Value::F64(i: OCamlInt64),
+            Value::V128(i: OCamlBytes),
+        }
+    }
+
+    // These functions must be exposed from OCaml with:
+    //   `Callback.register "interpret" interpret`
+    //
+    // In Rust, this function becomes:
+    //   `pub fn interpret(_: &mut OCamlRuntime, ...: OCamlRef<...>) -> BoxRoot<...>;`
+    ocaml! {
+        pub fn interpret(module: OCamlBytes, params: Option<OCamlList<Value>>) -> Result<OCamlList<Value>, String>;
+    }
+}
+
+/// Initialize a persistent OCaml runtime.
+///
+/// When used for fuzzing differentially with engines that also use signal
+/// handlers, this function provides a way to explicitly set up the OCaml
+/// runtime and configure its signal handlers.
+pub fn setup_ocaml_runtime() {
+    let _lock = INTERPRET.lock().unwrap();
+    OCamlRuntime::init_persistent();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn multiple() {
+        let module = wat::parse_file("tests/add.wat").unwrap();
+
+        let parameters1 = Some(vec![Value::I32(42), Value::I32(1)]);
+        let results1 = interpret(&module, parameters1.clone()).unwrap();
+
+        let parameters2 = Some(vec![Value::I32(1), Value::I32(42)]);
+        let results2 = interpret(&module, parameters2.clone()).unwrap();
+
+        assert_eq!(results1, results2);
+
+        let parameters3 = Some(vec![Value::I32(20), Value::I32(23)]);
+        let results3 = interpret(&module, parameters3.clone()).unwrap();
+
+        assert_eq!(results2, results3);
+    }
+
+    #[test]
+    fn oob() {
+        let module = wat::parse_file("tests/oob.wat").unwrap();
+        let results = interpret(&module, None);
+        assert_eq!(
+            results,
+            Err("Error(_, \"(Isabelle) trap: load\")".to_string())
+        );
+    }
+
+    #[test]
+    fn simd_not() {
+        let module = wat::parse_file("tests/simd_not.wat").unwrap();
+
+        let parameters = Some(vec![Value::V128(vec![
+            0, 255, 0, 0, 255, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0,
+        ])]);
+        let results = interpret(&module, parameters.clone()).unwrap();
+
+        assert_eq!(
+            results,
+            vec![Value::V128(vec![
+                255, 0, 255, 255, 0, 255, 255, 255, 255, 0, 255, 255, 255, 255, 255, 255
+            ])]
+        );
+    }
+
+    // See issue https://github.com/bytecodealliance/wasmtime/issues/4671.
+    #[test]
+    fn order_of_params() {
+        let module = wat::parse_file("tests/shr_s.wat").unwrap();
+
+        let parameters = Some(vec![Value::I32(1795123818), Value::I32(-2147483648)]);
+        let results = interpret(&module, parameters.clone()).unwrap();
+
+        assert_eq!(results, vec![Value::I32(1795123818)]);
+    }
+}
